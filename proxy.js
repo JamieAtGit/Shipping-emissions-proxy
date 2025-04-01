12d16c4b1bdeb7756b5b15b8ed1b7bb7b8f9bd60
JamieAtGit <jamie3.young@live.uwe.ac.uk>
2025-04-01 19:33:15 +0100

    Fix fetch import for node-fetch in CommonJS

--- proxy.js
+++ proxy.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
+// Fix for fetch in CommonJS
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const app = express();
 const PORT = 3001;
